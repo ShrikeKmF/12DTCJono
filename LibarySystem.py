397acd4999ca17afd17051b38d3f92c63d5f4e73
Shrike <Jschwass@outlook.com>
2022-02-24 13:54:28 +1300

    LibSys

--- LibarySystem.py
+++ LibarySystem.py
@@ -59,6 +59,59 @@ def add_book():
     print(f"{title} has been added to the book list")
 
 
+def find_user():
+    user_to_find = input("Enter the name of the user: ").title()
+    for user in user_list:
+        if user.name == user_to_find:
+            print(f"Hi {user_to_find}")
+            return user
+    print("Sorry no user with that name has been found")
+    return None
+
+
+def find_book():
+    book_to_find = input("Enter the name of the book: ").title()
+    for book in book_list:
+        if book.title == book_to_find:
+            print(f"The book '{book_to_find}' is in the catalogue")
+            return book
+    print("Sorry no book with that title is in the catalogue")
+    return None
+
+
+def lend_book():
+    user = find_user()
+    print()
+    if user:
+        book = find_book()
+        if book.available:
+            confirm = input("Type Y if you want to borrow this book: ").upper()
+            if confirm == "Y":
+                print(f"Book Title: {book.title} is now out on loan to"
+                      f" {user.name}")
+                book.available = False
+                book.borrower = user.name
+                user.Borrowed_books.append(book.title)
+        else:
+            print(f"Sorry, {book.title} is out on loan already")
+
+
+def return_book():
+    user = find_user()
+    print()
+    if user:
+        book = find_book()
+        if not book.available:
+            confirm = input("Type Y if you want to return this book: ").upper()
+            if confirm == "Y":
+                print(f"Book Title: {book.title} is now returned to the "
+                      f"library")
+                book.available = True
+                book.borrower = user.name
+                user.Borrowed_books.remove(book.title)
+        else:
+            print(f"Sorry, '{book.title}' is on loan to someone else")
+
 
 book_list = []
 user_list = []
@@ -73,7 +126,24 @@ User("Susan", "1011 Binary Rd")
 User("Paul", "25 Apple Tree Ave")
 User("Mary", "8 Moon Cres")
 
-add_book()
-print_info()
-add_user()
-print_user()
+new_action = True
+while new_action:
+    print("1. Lend Book")
+    print("2. Return Book")
+    print("3. Add User")
+    print("4. Add Book")
+    print("5. Exit")
+    choice = input("\n What would you like to do: ")
+    if choice == "1":
+        lend_book()
+    elif choice == "2":
+        return_book()
+    elif choice == "3":
+        add_user()
+    elif choice == "4":
+        add_book()
+    elif choice == "5":
+        new_action = False
+    else:
+        print("That is not a menu item please try again ")
+print("Thank you for using the Library System")
